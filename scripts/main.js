3b2f88da336acaafe237b8bedb17938b1f2ff33e
cziur <crisamziur@gmail.com>
2025-03-03 14:49:15 -0800

    Changed grid logic, added prompt for user input on number of boxes

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,43 @@
-function createGrid(row) {
-const gridContainer = document.querySelector('.gridContainer')
-const total = (row * row) + row
-const column = row + 1 // To make sure our grid is square and doesn't run across the whole page
+function createGrid(x) {
+    const gridContainer = document.querySelector('.gridContainer')
+    
 
-for (let i = 0; i < total; i++) {
+for (let row = 0; row < x; row++) {
+    for (let column = 0; column < x; column++) {
     const box = document.createElement('div');
-    if (i % column === 0) {
+    if (row === column + 1)  {                 // Once i is equal to column variable, it will increase the width to 100% and flex wrap will wrap our boxes to the next line
         box.style.cssText = 'border: 0; height: 0; width: 100%;'
     } else {
-    box.style.cssText = 'border: 1px solid black; height: 25px; width: 25px;'
+        box.style.cssText = `border: 1px solid black; height: ${960/x}px; width: ${960/x}px;`
     }
 
-    gridContainer.appendChild(box)
 
+    gridContainer.appendChild(box)
+    
     //Hover event
     box.addEventListener('mouseenter', function() {
         box.style.backgroundColor = 'black'
     });
-    box.addEventListener('mouseleave', function() {
-        box.style.backgroundColor = 'white;'
-    });
+}
 }
 
 }
-createGrid(16, 16)
+createGrid(prompt('How many squares?'))
+
+//Generate new grid button
+
+// function newGrid() {
+//     const button = document.querySelector('.getNewGrid')
+//     const askUser = button.addEventListener('click', function() {
+//         prompt('How many squares per side?')   
+    
+
+//     if (askUser < 3 || askUser > 100) {
+//         alert('Try a more reasonable number')
+//     } else {
+//         createGrid(askUser)
+//     }
+// })
+// }
+// newGrid()
 
